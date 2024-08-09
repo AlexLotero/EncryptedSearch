363fb2ddd5806611379d1a31d5dec19748e94c22
Alexander Lotero <yetiswerve@Alexanders-MacBook-Pro.local>
2024-08-09 11:47:57 -0600

    Initial Commit, standard fencrypt, as well as NN data

---
+++ text_classification.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import csv
+import pandas as pd
+import tensorflow as tf
+
+def build_model():
+    bbc_data = pd.read_csv("data/bbc_data.csv")
+    # print(bbc_data.head())
+    
+    # train-test split
+    
+
+    
+    model_stored = "/models"
+    return model_stored
+
+def main():
+    model_location = build_model()
+    # model_location = "EncryptedSearch/models/"
+    print("Text Classification Model built and available at %d", model_location)
+
+if __name__ == "__main__":
+    main()
